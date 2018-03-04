edbaa370826df38eeaecd51111fdc2da95a67fc3
YZ <1150681747@qq.com>
2018-03-04 21:49:05 +0800

    Dao interface方法哈哈哈哈

---
+++ src/main/java/dao/ClassDao.java
@@ -0,0 +1,8 @@
+package dao;
+
+/**
+ * @Author YZ
+ * @Date 2018/3/4
+ */
+public interface ClassDao {
+}
---
+++ src/main/java/dao/OrderClassDao.java
@@ -0,0 +1,9 @@
+package dao;
+
+/**
+ * @Author YZ
+ * @Date 2018/3/4
+ */
+public interface OrderClassDao {
+
+}
